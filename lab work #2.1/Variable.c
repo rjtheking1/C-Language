625356fd547aebc7c0bad87ce7087d246a78a257
mgiannopoulos24 <mariosgian_2002@yahoo.gr>
2024-07-02 11:37:22 +0300

    Small fix

--- lab work #2.1/Variable.c
+++ lab work #2.1/Variable.c
@@ -13,7 +13,7 @@ void main()
 	printf("x - y = %d\n",x-y);  
 	printf("x * y = %d\n",x*y);  
 	printf("x / y = %d\n",x/y); 
-	printf("x % y = %d\n",x%y);   
+	printf("x %% y = %d\n",x%y);   
 	
 	printf("x=%d\n",x);
 	printf("y=%d\n",y);
