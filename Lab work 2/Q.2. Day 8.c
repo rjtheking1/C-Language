c72f799bff8a62fccdad02ecf58f3458af3be325
Raj Patil <cryptomind1234@gmail.com>
2024-07-17 00:12:21 +0530

    Add files via upload

---
+++ Lab work 2/Q.2. Day 8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,z;
+	
+	printf("Enter the x number :");
+	scanf("%d",&x);
+	
+	printf("Enter the y number :");
+	scanf("%d",&y);
+	
+	printf("Enter the z number :");
+	scanf("%d",&z);        
+	
+	(x<y)?(x<z)?printf("x is maximum"):printf("z is maximum"):(y<z)?printf("y is maximum"):printf("z is maximum");
+	
+}
---
+++ Lab work 2/Q.3. Day 8.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,c,d;
+	
+	printf("Enter The Value Of a :");
+	scanf("%d",&a);
+	
+	printf("Enter The Value Of b :");
+	scanf("%d",&b);
+	
+	printf("Enter The Value Of c :");
+	scanf("%d",&c);
+	
+	printf("Enter The Value Of d :");
+	scanf("%d",&d);
+	
+	
+	if(a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+			{
+				printf("a is Maximum");
+			}
+			else
+			{
+				printf("d is Maximum");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is Maximum");
+			}
+			else
+			{
+				printf("d is Maximum");
+			}
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			if(b>d)
+			{
+				printf("b is Maximum");
+			}
+			else
+			{
+				printf("d is Maximum");
+			}
+		}
+		else
+		{
+			if(c>d)
+			{
+				printf("c is Maximum");
+			}
+			else
+			{
+				printf("d is Maximum");
+			}
+		}
+	}
+}
