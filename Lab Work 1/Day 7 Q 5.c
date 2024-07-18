d1536560d86816b294ab41f26253377c4b7e15bc
Raj Patil <cryptomind1234@gmail.com>
2024-07-18 11:48:10 +0530

    Day 7 Q 5.c

--- Lab Work 1/Day 7 Q 5.c
+++ Lab Work 1/Day 7 Q 5.c
@@ -3,12 +3,10 @@
 int  main()
 {
 	int x,reminder;
-	
 	printf("Enter the value of number =");
 	scanf("%d",&x);
 	
 	reminder=x%7;
-	
 	if(reminder==0)
 	{
 		printf("The given number is divisible by 7");
